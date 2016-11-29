d71276bb17008865abf5a2c6b9a91f88849266d2
Atif Aziz <code@raboof.com>
2016-11-29 21:43:46 +0100

    Test JobScheduler.Start

---
+++ test/JobSchedulerTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Kron.Tests
+{
+    using System.Threading;
+    using Xunit;
+
+    public class JobSchedulerTests
+    {
+        [Fact]
+        public async void Start()
+        {
+            var cts = new CancellationTokenSource();
+            var scheduler = JobScheduler<string>.Start(cts.Token);
+            Assert.NotNull(scheduler);
+            var task = scheduler.Task;
+            Assert.NotNull(task);
+            Assert.False(task.IsCompleted);
+            cts.Cancel();
+            await task;
+        }
+    }
+}
